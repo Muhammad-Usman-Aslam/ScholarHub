e1d04a5678c7f557561e98724756f41c7889f5f3
Muhammad Usman Aslam <muhammadusmanaslam76@gmail.com>
2026-07-05 06:49:09 +0500

    add vercel frontend update new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import useState from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./componets/Navbar";
 import Home from "./pages/Home";
 import BlogDetails from "./componets/BlogDetails";
@@ -7,29 +9,29 @@ import CountryBlogs from "./componets/CountryBlog";
 import CategoryBlogs from "./componets/CategoryBlog";
 import BlogAll from "./componets/Blog-All";
 import Contact from "./componets/Contact";
-import Footer from "./componets/Footer"
-import About from './componets/About'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./componets/About";
+import Footer from "./componets/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog/:id" element={<BlogDetails />} />
-          <Route path="/blog-details/:id" element={<BlogDetails />} />
-          <Route path="/search/:keyword" element={<SearchResult />} />
-          <Route path="/country/:country" element={<CountryBlogs />} />
+      <Navbar />
 
-<Route path="/category/:category" element={<CategoryBlogs />} />
-          <Route path="/blogs" element={<BlogAll />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About/>}/>
-        </Routes>
-        <Footer/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+        <Route path="/blog-details/:id" element={<BlogDetails />} />
+        <Route path="/search/:keyword" element={<SearchResult />} />
+        <Route path="/country/:country" element={<CountryBlogs />} />
+        <Route path="/category/:category" element={<CategoryBlogs />} />
+        <Route path="/blogs" element={<BlogAll />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
