01b95306053acf0c4f07d5a687e29eac64ee6513
Muhammad Usman Aslam <muhammadusmanaslam76@gmail.com>
2026-07-05 12:16:16 +0500

    Navbar Update

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -7,6 +7,8 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import './Navbar.css'
+
 
 function NavigationBar() {
   const [keyword, setKeyword] = useState("");
